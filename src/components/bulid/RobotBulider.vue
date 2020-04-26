e24addcdb2884a5fd3b7e075ba42bfe2030fb760
Jassar Al-Akhras <JAlakhras@blessedtreeit.com>
2020-04-27 01:05:48 +0300

    Conditionally Applying Styles with Style Bindings

--- src/components/bulid/RobotBulider.vue
+++ src/components/bulid/RobotBulider.vue
@@ -2,7 +2,7 @@
    <div class="content">
      <button class="add-to-cart" @click="addToCart">Add to cart</button>
     <div class="top-row">
-      <div class="top part">
+      <div class="top part" :style="headBorderStyle">
         <div  class="robot-name">{{selectedRobot.head.title}}
           <span class="sale" v-if="selectedRobot.head.onSale">Sale!</span>
         </div>
@@ -163,6 +163,10 @@ export default {
     },
   },
   computed :{
+    headBorderStyle()
+    {
+      return {border : this.selectedRobot.head.onSale ?'3px solid red': '3px solid #aaa'};
+    },
     selectedRobot() {
       return {
         head: availableParts.heads[this.selectedHeadIndex],
@@ -177,7 +181,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .part {
   position: relative;
   width:165px;

b34771e041198078e8274dfa2e89f8994f72a959
Jassar Al-Akhras <JAlakhras@blessedtreeit.com>
2020-04-28 02:53:42 +0300

    Using Nested Routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,11 @@ import Router from 'vue-router';
 import HomePage from '../components/home/HomePage.vue';
 import RobotBulider from '../components/bulid/RobotBulider.vue';
 import PartInfo from '../components/parts/PartInfo.vue';
+import BrowseParts from '../components/parts/BrowseParts.vue';
+import RobotHeads from '../components/parts/RobotHeads.vue';
+import RobotTorsos from '../components/parts/RobotTorsos.vue';
+import RobotArms from '../components/parts/RobotArms.vue';
+import RobotBases from '../components/parts/RobotBases.vue';
 
 Vue.use(Router);
 
@@ -17,7 +22,35 @@ export default new Router({
     {
       path: '/bulid',
       name: 'Bulid',
-      component : RobotBulider ,
+      component: RobotBulider,
+
+    },
+    {
+      path: '/parts/browse',
+      name: 'BrowseParts',
+      component: BrowseParts,
+      children: [
+        {
+          path:'heads',
+          name: 'BrowseHeads',
+          component :RobotHeads ,
+        },
+        {
+          path:'arms',
+          name: 'BrowseArams',
+          component :RobotArms ,
+        },
+        {
+          path:'torsos',
+          name: 'BrowseTorsos',
+          component :RobotTorsos ,
+        },
+        {
+          path:'bases',
+          name: 'BrowseBases',
+          component :RobotBases ,
+        }
+      ]
     },
     {
       path: '/parts/:partType/:id',
